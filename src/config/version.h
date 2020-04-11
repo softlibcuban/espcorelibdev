3758fa45145178a4c451c17597658ebf5d66e67a
Carlos Henry <chenryhabana205@gmail.com>
2020-04-11 01:35:10 -0400

    initial

---
+++ src/config/version.h
@@ -0,0 +1,5 @@
+#define APP_NAME                "espXcorelibdev"
+#define APP_VERSION             "0.0.1-dev"
+#define APP_AUTHOR              "chenryhabana205@gmail.com"
+#define APP_WEBSITE             "http://tinkerautomations.com"
+#define CFG_VERSION             1
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#else
+#include <WiFi.h>
+#endif
+
+void setup(){}
+void loop(){}
